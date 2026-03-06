bee6bf989a42a41f5711eda47b38a9b380d51951
Vinícius <viniciusdiller@hotmail.com>
2026-03-05 23:34:47 -0300

    feat: Rota de Pegar Post por ID

--- src/presentation/controllers/post.controller.ts
+++ src/presentation/controllers/post.controller.ts
@@ -1,40 +1,60 @@
-import { Controller, Get, Post, Body, UseGuards, Request } from '@nestjs/common';
-import { ApiTags, ApiOperation, ApiCreatedResponse, ApiOkResponse, ApiBearerAuth } from '@nestjs/swagger';
-import { PostApplication } from 'application/applications/posts.Application';
-import { CreatePostRequestDto } from 'presentation/dto/request/createPostsRequestDto';
-import { PostResponseDto } from '../dto/response/postResponse.dto';
-import { JwtAuthGuard } from 'application/guards/jwtAuth.Guard';
-import { RolesGuard } from 'application/guards/roles.Guard';
-import { Roles } from 'application/decorators/rolesDecorator';
-import { RequestWithUser } from 'data/interfaces/IRequestWithUser.Interface';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  UseGuards,
+  Request,
+  Param,
+} from "@nestjs/common";
+import {
+  ApiTags,
+  ApiOperation,
+  ApiCreatedResponse,
+  ApiOkResponse,
+  ApiBearerAuth,
+} from "@nestjs/swagger";
+import { PostApplication } from "application/applications/posts.Application";
+import { CreatePostRequestDto } from "presentation/dto/request/createPostsRequestDto";
+import { PostResponseDto } from "../dto/response/postResponse.dto";
+import { JwtAuthGuard } from "application/guards/jwtAuth.Guard";
+import { RolesGuard } from "application/guards/roles.Guard";
+import { Roles } from "application/decorators/rolesDecorator";
+import { RequestWithUser } from "data/interfaces/IRequestWithUser.Interface";
 
 @ApiBearerAuth()
-@ApiTags('Posts')
-@Controller('posts')
+@ApiTags("Posts")
+@Controller("posts")
 export class PostController {
   constructor(private readonly postApplication: PostApplication) {}
 
   @Get()
-  @ApiOperation({ summary: 'Lista todas as notícias do blog' })
+  @ApiOperation({ summary: "Lista todas as notícias do blog" })
   @ApiOkResponse({ type: [PostResponseDto] })
   async findAll(): Promise<PostResponseDto[]> {
     return await this.postApplication.listAllPosts();
   }
 
+  @Get(":id")
+  @ApiOperation({ summary: "Busca um post específico pelo ID" })
+  @ApiOkResponse({ type: PostResponseDto })
+  async findOne(@Param("id") id: string): Promise<PostResponseDto> {
+    return await this.postApplication.getPostById(Number(id));
+  }
+
   @Post()
   @UseGuards(JwtAuthGuard, RolesGuard)
-  @Roles('autor', 'admin')
+  @Roles("autor", "admin")
   @ApiBearerAuth()
   async create(
-    @Body() createPostDto: CreatePostRequestDto, 
-    @Request() req: RequestWithUser 
+    @Body() createPostDto: CreatePostRequestDto,
+    @Request() req: RequestWithUser,
   ): Promise<PostResponseDto> {
-    
     const authorId = req.user.sub;
 
     return await this.postApplication.createPost({
       ...createPostDto,
-      authorId: authorId 
-      });
-    }
+      authorId: authorId,
+    });
   }
+}
